91dbc2be51e2a3000b75e504a51b2d531e6fad79
luiz-cesar-ti <luiz.cesar1995@gmail.com>
2026-01-01 11:27:38 -0300

    feat: add Vite configuration for React with build optimizations and manual chunking.

--- sistema-agendamento/vite.config.ts
+++ sistema-agendamento/vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Aumenta o limite de aviso para 1000kb
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['react', 'react-dom', 'react-router-dom'],
+          'ui': ['lucide-react', 'recharts', 'clsx', 'tailwind-merge'],
+          'pdf': ['html2pdf.js'],
+          'supabase': ['@supabase/supabase-js']
+        }
+      }
+    }
+  }
 })
